2184e884e1cd0851a1462f984d4452b543e7befd
arindambala <252558374+arindambala@users.noreply.github.com>
2026-08-14 23:48:40 +0530

    Initial commit

--- C/main.c
+++ C/main.c
@@ -12,6 +12,8 @@ int main()
 
     printf("There once was a man named John Doe.\n");
     printf("He was 35 years old.\n");
+    printf("He really liked the name John Doe,\n");
+    printf("but did not like being 35.\n");
 
     return 0;
 }
