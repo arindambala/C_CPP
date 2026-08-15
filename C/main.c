3fc7d05b881f1a3426705f5ae66d2c9b9d6df36a
arindambala <252558374+arindambala@users.noreply.github.com>
2026-08-15 23:00:30 +0530

    Initial commit

--- C/main.c
+++ C/main.c
@@ -15,6 +15,7 @@ int main()
 
     printf("There once was a man named %s.\n", characterName);
     printf("He was %d years old.\n", characterAge);
+    characterAge = 40;
     printf("He really liked the name %s,\n", characterName);
     printf("but did not like being %d.\n", characterAge);
 
