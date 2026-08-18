e757dd41ccf380d22ea40d3ff66f4b40586229ac
arindambala <252558374+arindambala@users.noreply.github.com>
2026-08-18 23:13:58 +0530

    Updated code

--- C/Projects/madlibs.c
+++ C/Projects/madlibs.c
@@ -11,3 +11,20 @@ void clear_input_buffer()
     int ch;
     while ((ch = getchar()) != '\n' && ch != EOF);
 }
+
+int main()
+{
+    char adjectiveOne[MAX_STR_LEN];
+    char noun[MAX_STR_LEN];
+    char verb[MAX_STR_LEN];
+    char place[MAX_STR_LEN];
+    char animal[MAX_STR_LEN];
+    char adjectiveTwo[MAX_STR_LEN];
+    char exclamation[MAX_STR_LEN];
+    char food[MAX_STR_LEN];
+
+    printf("=========================================\n");
+    printf("         WELCOME TO MAD LIBS!          \n");
+    printf("=========================================\n");
+    printf("Please provide the following words to create a story -> \n\n");
+}
