6ce32bce7392cb7505dec93899b3d3cf0b50d052
arindambala <252558374+arindambala@users.noreply.github.com>
2026-08-24 23:29:09 +0530

    Initial commit

--- C/FileHandling/main.c
+++ C/FileHandling/main.c
@@ -4,12 +4,36 @@
 int main()
 {
     FILE *file_ptr = fopen("example.txt", "w");
-    FILE *file_ptrOne = fopen("example.txt", "a");
 
     fprintf(file_ptr, "John Doe, Sales\nJane Doe, Marketing\nJack, Accounting\nJill, Receptionist");
-    fprintf(file_ptrOne, "\nJade, Customer Service");
 
     fclose(file_ptr);
+
+    FILE *file_ptrOne = fopen("example.txt", "a");
+
+    fprintf(file_ptrOne, "\nJade, Customer Service");
+
+    fclose(file_ptrOne);
+
+    char line[255];
+    FILE *file_ptrTwo = fopen("example.txt", "r");
+
+    /*
+    
+    fgets(line, 255, file_ptrTwo);
+    printf("%s", line);
+    fgets(line, 255, file_ptrTwo);
+    printf("%s", line); 
+    
+    */
+
+    if (file_ptrTwo == NULL) return 1;
+    printf("\n");
+
+    while (fgets(line, sizeof(line), file_ptrTwo) != NULL) { printf("%s", line); }
+    printf("\n");
+
+    fclose(file_ptrTwo);
 
     return 0;
 }
