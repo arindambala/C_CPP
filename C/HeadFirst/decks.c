b9d6b4c125af4f84f22d6801c266b820cfc6b1b6
arindambala <252558374+arindambala@users.noreply.github.com>
2026-08-30 23:46:38 +0530

    Decks

---
+++ C/HeadFirst/decks.c
@@ -0,0 +1,9 @@
+/*
+
+* Program to calculate the number of cards in the shoe - Head First C.
+* This code is from the book mentioned above - Page 5.
+* (c) 2014, The College Blackjack Team / Vegas Public License.
+
+*/
+
+#include <stdio.h>
