0884b1a777d46ded7fe298408e44da6e8839be8c
arindambala <252558374+arindambala@users.noreply.github.com>
2026-08-31 23:48:22 +0530

    Exercise 1

--- C/HeadFirst/Exercises/1_describe.c
+++ C/HeadFirst/Exercises/1_describe.c
@@ -6,5 +6,17 @@ int main()
 {
     // Describe what you think the code does
 
+    int card_count = 11;
+    if (card_count > 10)
+        puts("The deck is hot. Increase bet!");
+    
+    /*
+    
+    Create an integer variable and set it to 11..
+    Is the count more than 10?
+    if so, display a message on the command prompt.
+
+    */
+
     return 0;
 }
