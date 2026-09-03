76b577dd46e02d2c3cf70e560d28e2baf77c4aab
arindambala <252558374+arindambala@users.noreply.github.com>
2026-09-03 23:40:20 +0530

    Exercise 2

--- C/HeadFirst/Exercises/2_switch.c
+++ C/HeadFirst/Exercises/2_switch.c
@@ -13,6 +13,20 @@ int main()
 
     int val = 0;
 
+    // switch-case statements
+    switch (card_name[0])
+    {
+        case 'K':
+        case 'Q':
+        case 'J':
+            val = 10;
+            break;
+        case 'A':
+            val = 11;
+            break;
+        default:
+            val = atoi(card_name);
+    }
     printf("The card value is: %i\n", val);
 
     /*
