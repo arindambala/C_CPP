f89328e39f4efddbaa83e18426624fe44a083678
arindambala <252558374+arindambala@users.noreply.github.com>
2026-09-07 23:39:13 +0530

    Memory

---
+++ C/HeadFirst/pointer.c
@@ -0,0 +1,14 @@
+/*
+
+* Program to track the location of players using latitudes & longitudes - Head First C.
+* This code is from the book mentioned above - Page 50.
+* If a player wants to travel southeast; Latitude ↑ Longitude ↓.
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    return 0;
+}
