4071dacc4a82bb9251c057cdde21c61dfade31c9
arindambala <252558374+arindambala@users.noreply.github.com>
2026-09-17 23:00:34 +0530

    Location

--- C/HeadFirst/sail.c
+++ C/HeadFirst/sail.c
@@ -1,7 +1,7 @@
 /*
 
 * Program to track the location of players using latitudes & longitudes - Head First C.
-* This code is from the book mentioned above - Page 5.
+* This code is from the book mentioned above - Page 50.
 * If a player wants to travel southeast; Latitude ↑ Longitude ↓.
 
 */
