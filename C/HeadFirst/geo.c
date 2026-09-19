8fddc037c8eed27aa8d28c131e4ac416ae294699
arindambala <252558374+arindambala@users.noreply.github.com>
2026-09-19 23:59:07 +0530

    File

--- C/HeadFirst/geo.c
+++ C/HeadFirst/geo.c
@@ -17,6 +17,16 @@ int main()
 
     puts("data = [");
 
+    while (scanf("%f,%f,%79[^\n]", &latitude, &longitude, info) == 3)
+    {
+        if (started)
+            printf(",\n");
+        else
+            started = 1;
+        
+        printf("\n{latitude: %f, longitude: %f, info: '%s'}", latitude, longitude, info);
+    }
+
     puts("\n]");
 
     return 0;
