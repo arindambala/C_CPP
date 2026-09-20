bc29594feefc4ff9048c3b8655a652ffc26c8af3
arindambala <252558374+arindambala@users.noreply.github.com>
2026-09-20 23:21:51 +0530

    Data

---
+++ C/HeadFirst/Geo/gpsdata.js
@@ -0,0 +1,12 @@
+data = [
+
+{latitude: 42.363400, longitude: -71.098801, info: 'MIT'},
+
+{latitude: 37.774899, longitude: -122.419403, info: 'San Francisco'},
+
+{latitude: 51.507401, longitude: -0.127800, info: 'London'},
+
+{latitude: 40.712799, longitude: -74.005997, info: 'New York'},
+
+{latitude: 28.613899, longitude: 77.209000, info: 'New Delhi'}
+]
