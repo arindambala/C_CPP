cb3282473dab49b27c136fdbe8ac24be8fa7cb92
arindambala <252558374+arindambala@users.noreply.github.com>
2026-09-23 23:53:28 +0530

    Stream

---
+++ C/HeadFirst/category.c
@@ -0,0 +1,16 @@
+/*
+
+* Program to store & produce data to different files - Head First C.
+* This code is from the book mentioned above - Page 140.
+* Display it - JSON format.
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    return 0;
+}
