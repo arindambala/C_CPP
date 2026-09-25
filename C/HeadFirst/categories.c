e47d43fd91823cc14daee34c16d843793d1d5cfa
arindambala <252558374+arindambala@users.noreply.github.com>
2026-09-25 19:35:12 +0530

    Commands

---
+++ C/HeadFirst/categories.c
@@ -0,0 +1,16 @@
+/*
+
+* Program to store & produce data the way a user would want it to be split up - Head First C.
+* This code is from the book mentioned above - Page 144.
+* Ability to pass - CLI arguments (command-line argc, argv)
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    return 0;
+}
